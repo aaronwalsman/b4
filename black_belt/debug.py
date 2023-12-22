e12331d6bd4e2d37141c4121a4cedf53046935b0
Aaron Walsman <awalsman@cs.washington.edu>
2023-12-21 18:35:41 -0800

    adding debug.py before removing it

--- black_belt/debug.py
+++ black_belt/debug.py
@@ -1,9 +1,17 @@
-import numpy
+import numpy as np
 
-from black_belt.ne import equilibrium
+#from black_belt.ne import equilibrium
+from black_belt.ne import lp_solve_zero_sum
+import json
 
-game = numpy.load('dump_802416.npy')
+#game = numpy.load('dump_802416.npy')
+#policy, value = equilibrium(game)
 
-policy, value = equilibrium(game)
+data = json.load(open('fail_log_18.json'))
+game = np.array(data['game'])
+
+print(game)
+
+policy, value = lp_solve_zero_sum(game)
 
 print(policy, value)
